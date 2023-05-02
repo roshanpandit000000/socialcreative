3014135c3957e566a4f81a259b16f0e792951496
roshan pandit <roshanpandit000000@gmail.com>
2023-05-02 18:12:05 +0530

    social creative product render on database error

--- components/HomePage.js
+++ components/HomePage.js
@@ -2,14 +2,17 @@ import React from "react";
 import Layout from "./Layout";
 import ProductItem from "./ProductItem";
 import data from "@/utils/data";
+import Product from '../models/Product';
+import db from '../utils/db';
+import { Store } from '../utils/Store';
 
-export default function HomePage() {
+export default function HomePage({products}) {
   return (
     <>
       <Layout title="Home Page">
         <div className="container py-28 mx-auto px-7 sm:px-7 lg:px-0 ">
           <div class="grid grid-cols-2 md:grid-cols-6 gap-6">
-            {data.products.map((product) => (
+            {products.map((product) => (
               <ProductItem product={product} key={product.slug}></ProductItem>
             ))}
           </div>
@@ -17,4 +20,14 @@ export default function HomePage() {
       </Layout>
     </>
   );
+}
+
+export async function getServerSideProps() {
+  await db.connect();
+  const products = await Product.find().lean();
+  return {
+    props: {
+      products: products.map(db.convertDocToObj),
+    },
+  };
 }
